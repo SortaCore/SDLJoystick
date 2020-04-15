42f987a83d50bb2525eaaa90c86fede65e15a094
PkR <pkr@pkr.pkr>
2020-04-16 02:09:13 +0900

    Upload without ToInst/Help

---
+++ Main.cpp
@@ -0,0 +1,271 @@
+// ============================================================================
+//
+// This file are where the Conditions/Actions/Expressions are defined.
+//
+// ============================================================================
+
+// Common Include
+#include "common.h"
+#include "SDL.h"
+
+
+//Variables
+
+bool initialize = false;
+
+// Quick memo: content of the eventInformations arrays
+// ---------------------------------------------------
+// Menu ID
+// String ID
+// Code
+// Flags
+// Number_of_parameters
+// Parameter_type [Number_of_parameters]
+// Parameter_TitleString [Number_of_parameters]
+
+// Definitions of parameters for each condition
+short conditionsInfos[]=
+		{
+		IDMN_ISCONNECTED, M_ISCONNECTED, CND_ISCONNECTED, EVFLAGS_ALWAYS + EVFLAGS_NOTABLE, 1, PARAM_EXPRESSION, M_CND_JOY,
+		IDMN_BUTTONPRESSED, M_BUTTONPRESSED, CND_BUTTONPRESSED, EVFLAGS_ALWAYS + EVFLAGS_NOTABLE, 2, PARAM_EXPRESSION, PARAM_EXPRESSION, M_CND_JOY, M_CND_BUTTON,
+		IDMN_BUTTONHELDDOWN, M_BUTTONHELDDOWN, CND_BUTTONHELDDOWN, EVFLAGS_ALWAYS + EVFLAGS_NOTABLE, 2, PARAM_EXPRESSION, PARAM_EXPRESSION, M_CND_JOY, M_CND_BUTTON,
+		IDMN_BUTTONRELEASED, M_BUTTONRELEASED, CND_BUTTONRELEASED, EVFLAGS_ALWAYS + EVFLAGS_NOTABLE, 2, PARAM_EXPRESSION, PARAM_EXPRESSION, M_CND_JOY, M_CND_BUTTON,
+		IDMN_ANYBUTTONPRESSED, M_ANYBUTTONPRESSED, CND_ANYBUTTONPRESSED, EVFLAGS_ALWAYS + EVFLAGS_NOTABLE, 2, PARAM_EXPRESSION, PARAM_EXPRESSION, M_CND_JOY,
+		IDMN_ANYBUTTONHELDDOWN, M_ANYBUTTONHELDDOWN, CND_ANYBUTTONHELDDOWN, EVFLAGS_ALWAYS + EVFLAGS_NOTABLE, 1, PARAM_EXPRESSION, M_CND_JOY,
+		IDMN_ANYBUTTONRELEASED, M_ANYBUTTONRELEASED, CND_ANYBUTTONRELEASED, EVFLAGS_ALWAYS + EVFLAGS_NOTABLE, 1, PARAM_EXPRESSION, M_CND_JOY,
+		};
+
+// Definitions of parameters for each action
+short actionsInfos[] =
+		{ 0 };
+
+// Definitions of parameters for each expression
+short expressionsInfos[]=
+		{
+		IDMN_NUMJOYS, M_NUMJOYS, EXP_NUMJOYS, 0, 0,
+		IDMN_GETAXIS, M_GETAXIS, EXP_GETAXIS, 0, 2, EXPPARAM_LONG, EXPPARAM_LONG, 0, 0,
+		IDMN_GETBUTTON, M_GETBUTTON, EXP_GETBUTTON, 0, 2, EXPPARAM_LONG, EXPPARAM_LONG, 0, 0,
+		IDMN_GETHAT, M_GETHAT, EXP_GETHAT, 0, 2, EXPPARAM_LONG, EXPPARAM_LONG, 0, 0,
+		IDMN_GETBALLX, M_GETBALLX, EXP_GETBALLX, 0, 2, EXPPARAM_LONG, EXPPARAM_LONG, 0, 0,
+		IDMN_GETBALLY, M_GETBALLY, EXP_GETBALLY, 0, 2, EXPPARAM_LONG, EXPPARAM_LONG, 0, 0,
+		IDMN_NUMAXES, M_NUMAXES, EXP_NUMAXES, 0, 1, EXPPARAM_LONG, 0,
+		IDMN_NUMBUTTONS, M_NUMBUTTONS, EXP_NUMBUTTONS, 0, 1, EXPPARAM_LONG, 0,
+		IDMN_NUMHATS, M_NUMHATS, EXP_NUMHATS, 0, 1, EXPPARAM_LONG, 0,
+		IDMN_NUMBALLS, M_NUMBALLS, EXP_NUMBALLS, 0, 1, EXPPARAM_LONG, 0,
+		IDMN_DEVICEGUID, M_DEVICEGUID, EXP_DEVICEGUID, EXPFLAG_STRING, 1, EXPPARAM_LONG, 0,
+		IDMN_DEVICENAME, M_DEVICENAME, EXP_DEVICENAME, EXPFLAG_STRING, 1, EXPPARAM_LONG, 0,
+		};
+
+// ============================================================================
+//
+// CONDITION ROUTINES
+// 
+// ============================================================================
+
+long WINAPI DLLExport IsConnected(LPRDATA rdPtr, long param1, long param2)
+{
+	return rdPtr->SDL_Data[param1].connected;
+}
+
+long WINAPI DLLExport ButtonPressed(LPRDATA rdPtr, long param1, long param2)
+{
+	if (rdPtr->SDL_Data[param1].held_buttons[param2] && !rdPtr->SDL_Data[param1].held_buttons_last[param2]) return 1;
+	else return 0;
+}
+
+long WINAPI DLLExport ButtonHeldDown(LPRDATA rdPtr, long param1, long param2)
+{
+	if (rdPtr->SDL_Data[param1].held_buttons[param2]) return 1;
+	else return 0;
+}
+
+long WINAPI DLLExport ButtonReleased(LPRDATA rdPtr, long param1, long param2)
+{
+	if (!rdPtr->SDL_Data[param1].held_buttons[param2] && rdPtr->SDL_Data[param1].held_buttons_last[param2]) return 1;
+	else return 0;
+}
+
+long WINAPI DLLExport AnyButtonPressed(LPRDATA rdPtr, long param1, long param2)
+{
+	for (int i = 0; i < 32; i++)
+	{
+		if (rdPtr->SDL_Data[param1].held_buttons[i] && !rdPtr->SDL_Data[param1].held_buttons_last[i]) return 1;
+	}
+	return 0;
+}
+
+long WINAPI DLLExport AnyButtonHeldDown(LPRDATA rdPtr, long param1, long param2)
+{
+	for (int i = 0; i < 32; i++)
+	{
+		if (rdPtr->SDL_Data[param1].held_buttons[i]) return 1;
+	}
+	return 0;
+}
+
+long WINAPI DLLExport AnyButtonReleased(LPRDATA rdPtr, long param1, long param2)
+{
+	for (int i = 0; i < 32; i++)
+	{
+		if (!rdPtr->SDL_Data[param1].held_buttons[i] && rdPtr->SDL_Data[param1].held_buttons_last[i]) return 1;
+	}
+	return 0;
+}
+// ============================================================================
+//
+// ACTIONS ROUTINES
+// 
+// ============================================================================
+
+// ============================================================================
+//
+// EXPRESSIONS ROUTINES
+// 
+// ============================================================================
+
+long WINAPI DLLExport GetAxis(LPRDATA rdPtr, long param1)
+{
+	long joy = CNC_GetFirstExpressionParameter(rdPtr, param1, TYPE_INT);
+	long axis = CNC_GetNextExpressionParameter(rdPtr, param1, TYPE_INT);
+	return (long)(rdPtr->SDL_Data[joy].axis[axis]);
+}
+
+long WINAPI DLLExport GetButton(LPRDATA rdPtr, long param1)
+{
+	long joy = CNC_GetFirstExpressionParameter(rdPtr, param1, TYPE_INT);
+	long button = CNC_GetNextExpressionParameter(rdPtr, param1, TYPE_INT);
+	return (long)(rdPtr->SDL_Data[joy].held_buttons[button]);
+}
+
+long WINAPI DLLExport GetHat(LPRDATA rdPtr, long param1)
+{
+	long joy = CNC_GetFirstExpressionParameter(rdPtr, param1, TYPE_INT);
+	long hat = CNC_GetNextExpressionParameter(rdPtr, param1, TYPE_INT);
+	return (long)(rdPtr->SDL_Data[joy].hat[hat]);
+}
+
+long WINAPI DLLExport GetBallX(LPRDATA rdPtr, long param1)
+{
+	long joy = CNC_GetFirstExpressionParameter(rdPtr, param1, TYPE_INT);
+	long ball = CNC_GetNextExpressionParameter(rdPtr, param1, TYPE_INT);
+	return (long)(rdPtr->SDL_Data[joy].ball_x[ball]);
+}
+
+long WINAPI DLLExport GetBallY(LPRDATA rdPtr, long param1)
+{
+	long joy = CNC_GetFirstExpressionParameter(rdPtr, param1, TYPE_INT);
+	long ball = CNC_GetNextExpressionParameter(rdPtr, param1, TYPE_INT);
+	return (long)(rdPtr->SDL_Data[joy].ball_y[ball]);
+}
+
+long WINAPI DLLExport NumAxes(LPRDATA rdPtr,long param1)
+{
+	long joy = CNC_GetFirstExpressionParameter(rdPtr, param1, TYPE_INT);
+	return (long)(rdPtr->SDL_Data[joy].num_axes);
+}
+
+long WINAPI DLLExport NumButtons(LPRDATA rdPtr,long param1)
+{
+	long joy = CNC_GetFirstExpressionParameter(rdPtr, param1, TYPE_INT);
+	return (long)(rdPtr->SDL_Data[joy].num_buttons);
+}
+
+long WINAPI DLLExport NumHats(LPRDATA rdPtr, long param1)
+{
+	long joy = CNC_GetFirstExpressionParameter(rdPtr, param1, TYPE_INT);
+	return (long)(rdPtr->SDL_Data[joy].num_hats);
+}
+
+long WINAPI DLLExport NumBalls(LPRDATA rdPtr, long param1)
+{
+	long joy = CNC_GetFirstExpressionParameter(rdPtr, param1, TYPE_INT);
+	return (long)(rdPtr->SDL_Data[joy].num_balls);
+}
+
+long WINAPI DLLExport NumJoysticks(LPRDATA rdPtr,long param1)
+{
+	short numjoys = 0;
+ 
+	for (int joy = 0; joy < 16; joy++)
+	{
+		if (rdPtr->SDL_Data[joy].connected) numjoys++;
+	}
+	
+	return numjoys;
+}
+
+long WINAPI DLLExport GetDeviceGUID(LPRDATA rdPtr, long param1)
+{
+	SDL_JoystickGUID guid;
+	LPTSTR pszName = ("");
+	long joy = CNC_GetFirstExpressionParameter(rdPtr, param1, TYPE_LONG);
+	if (rdPtr->SDL != nullptr && rdPtr->SDL_Data[joy].connected)
+	{
+		pszName = (LPTSTR)callRunTimeFunction(rdPtr, RFUNCTION_GETSTRINGSPACE_EX, 0, 33 * sizeof(TCHAR));
+		if (pszName != NULL)
+		{
+			guid = SDL_JoystickGetGUID(rdPtr->SDL_Data[joy].joystick);
+			SDL_JoystickGetGUIDString(guid, pszName, 33);
+		}
+	}
+	rdPtr->rHo.hoFlags |= HOF_STRING;
+	return (long)pszName;
+}
+
+long WINAPI DLLExport GetDeviceName(LPRDATA rdPtr, long param1)
+{
+	LPTSTR pszName = ("");
+	long joy = CNC_GetFirstExpressionParameter(rdPtr, param1, TYPE_LONG);
+	if (rdPtr->SDL != nullptr && rdPtr->SDL_Data[joy].connected)
+	{
+		pszName = (LPTSTR)callRunTimeFunction(rdPtr, RFUNCTION_GETSTRINGSPACE_EX, 0, 512 * sizeof(TCHAR));
+		if (pszName != NULL)
+		{
+			sprintf(pszName, "%s", SDL_JoystickName(rdPtr->SDL_Data[joy].joystick));
+		}
+	}
+	rdPtr->rHo.hoFlags |= HOF_STRING;
+	return (long)pszName;
+}
+
+// ----------------------------------------------------------
+// Condition / Action / Expression jump table
+// ----------------------------------------------------------
+// Contains the address inside the extension of the different
+// routines that handle the action, conditions and expressions.
+// Located at the end of the source for convinience
+// Must finish with a 0
+//
+long (WINAPI * ConditionJumps[])(LPRDATA rdPtr, long param1, long param2) = 
+			{ 
+			IsConnected,
+			ButtonPressed,
+			ButtonHeldDown,
+			ButtonReleased,
+			AnyButtonPressed,
+			AnyButtonHeldDown,
+			AnyButtonReleased,
+			0
+			};
+	
+short (WINAPI * ActionJumps[])(LPRDATA rdPtr, long param1, long param2) =
+			{
+			0
+			};
+
+long (WINAPI * ExpressionJumps[])(LPRDATA rdPtr, long param) = 
+			{     
+			NumJoysticks,
+			GetAxis,
+			GetButton,
+			GetHat,
+			GetBallX,
+			GetBallY,
+			NumAxes,
+			NumButtons,
+			NumHats,
+			NumBalls,
+			GetDeviceName,
+			GetDeviceGUID,
+			0
+			};
---
+++ Runtime.cpp
@@ -0,0 +1,278 @@
+// ============================================================================
+//
+// This file contains routines that are handled during the Runtime.
+//
+// Including creating, display, and handling your object.
+// 
+// ============================================================================
+
+// Common Include
+#include "common.h"
+#include "SDL.h"
+
+// --------------------
+// GetRunObjectDataSize
+// --------------------
+// Returns the size of the runtime datazone of the object
+// 
+ushort WINAPI DLLExport GetRunObjectDataSize(fprh rhPtr, LPEDATA edPtr)
+{
+	return(sizeof(RUNDATA));
+}
+
+// ---------------
+// CreateRunObject
+// ---------------
+// The routine where the object is actually created
+// 
+
+short WINAPI DLLExport CreateRunObject(LPRDATA rdPtr, LPEDATA edPtr, fpcob cobPtr)
+{
+	TCHAR SDLPath[400];
+	TCHAR rootdir[MAX_PATH];
+	TCHAR ErrorMsg[400];
+	DWORD dwError = 0;
+	//Reset joystick IDs
+	for (int i = 0; i < 16; i++)
+	{
+		rdPtr->SDL_Data[i].joy_id = -1;
+	}
+	//Get path to DLL
+	GetCurrentDirectory(MAX_PATH - 1, rootdir);
+	sprintf(SDLPath, "%s\\%s", rootdir, "SDL2.DLL");
+	//Open SDL library
+	rdPtr->SDL = LoadLibrary(SDLPath);
+	dwError = GetLastError();
+	if (rdPtr->SDL == nullptr)
+	{
+		sprintf(ErrorMsg, "Error loading library %s: code %d.", SDLPath, dwError);
+		MessageBoxA(nullptr, ErrorMsg,
+			"SDL Load Error", MB_OK | MB_ICONERROR);
+		return 0;
+	}
+	int init;
+	if ((init = SDL_Init(SDL_INIT_GAMECONTROLLER | SDL_INIT_HAPTIC | SDL_INIT_EVENTS)) != 0)
+	{
+		sprintf(ErrorMsg, "Error initializing SDL: code %d.", SDL_GetError());
+		MessageBoxA(nullptr, ErrorMsg,
+			"SDL Init Error", MB_OK | MB_ICONERROR);
+		return 0;
+	}
+	SDL_JoystickEventState(SDL_ENABLE);
+	return 0;
+}
+
+// ----------------
+// DestroyRunObject
+// ----------------
+// Destroys the run-time object
+// 
+short WINAPI DLLExport DestroyRunObject(LPRDATA rdPtr, long fast)
+{
+	// No errors
+	return 0;
+}
+
+void CloseJoystick(LPRDATA rdPtr, int joy)
+{
+	for (int i = 0; i < 8; i++)
+	{
+		rdPtr->SDL_Data[joy].axis[i] = 0;
+	}
+	for (int i = 0; i < 32; i++)
+	{
+		rdPtr->SDL_Data[joy].held_buttons[i] = false;
+		rdPtr->SDL_Data[joy].held_buttons_last[i] = false;
+	}
+	for (int i = 0; i < 4; i++)
+	{
+		rdPtr->SDL_Data[joy].hat[i] = 0;
+	}
+	for (int i = 0; i < 4; i++)
+	{
+		rdPtr->SDL_Data[joy].ball_x[i] = 0;
+		rdPtr->SDL_Data[joy].ball_y[i] = 0;
+	}
+	rdPtr->SDL_Data[joy].joy_id = -1;
+	rdPtr->SDL_Data[joy].num_axes = 0;
+	rdPtr->SDL_Data[joy].num_buttons = 0;
+	rdPtr->SDL_Data[joy].num_hats = 0;
+	rdPtr->SDL_Data[joy].num_balls = 0;
+	if (rdPtr->SDL_Data[joy].haptic != nullptr)
+	{
+		SDL_HapticClose(rdPtr->SDL_Data[joy].haptic);
+		rdPtr->SDL_Data[joy].haptic = nullptr;
+	}
+	if (rdPtr->SDL_Data[joy].joystick != nullptr)
+	{
+		SDL_JoystickClose(rdPtr->SDL_Data[joy].joystick);
+		rdPtr->SDL_Data[joy].joystick = nullptr;
+	}
+	rdPtr->SDL_Data[joy].connected = false;
+}
+
+void OpenJoystick(LPRDATA rdPtr, int joy)
+{
+	if (rdPtr->SDL_Data[joy].connected) CloseJoystick(rdPtr, joy);
+	rdPtr->SDL_Data[joy].joystick = SDL_JoystickOpen(joy);
+	if (rdPtr->SDL_Data[joy].joystick == nullptr)
+	{
+		rdPtr->SDL_Data[joy].connected = false;
+		rdPtr->SDL_Data[joy].joy_id = -1;
+		return;
+	}
+	rdPtr->SDL_Data[joy].joy_id = joy;
+	rdPtr->SDL_Data[joy].num_axes = SDL_JoystickNumAxes(rdPtr->SDL_Data[joy].joystick);
+	rdPtr->SDL_Data[joy].num_buttons = SDL_JoystickNumButtons(rdPtr->SDL_Data[joy].joystick);
+	rdPtr->SDL_Data[joy].num_hats = SDL_JoystickNumHats(rdPtr->SDL_Data[joy].joystick);
+	rdPtr->SDL_Data[joy].num_balls = SDL_JoystickNumBalls(rdPtr->SDL_Data[joy].joystick);
+	rdPtr->SDL_Data[joy].haptic = SDL_HapticOpenFromJoystick(rdPtr->SDL_Data[joy].joystick);
+	if (rdPtr->SDL_Data[joy].haptic == nullptr)
+	{
+		rdPtr->SDL_Data[joy].connected = true;
+		return;
+	}
+	if (!SDL_HapticRumbleSupported(rdPtr->SDL_Data[joy].haptic))
+	{
+		SDL_HapticClose(rdPtr->SDL_Data[joy].haptic);
+		rdPtr->SDL_Data[joy].haptic = nullptr;
+	}
+	rdPtr->SDL_Data[joy].connected = true;
+}
+
+// ----------------
+// HandleRunObject
+// ----------------
+// Called (if you want) each loop, this routine makes the object live
+// 
+short WINAPI DLLExport HandleRunObject(LPRDATA rdPtr)
+{
+	if (rdPtr->SDL == nullptr) return REFLAG_DISPLAY;
+	SDL_JoystickUpdate();
+	SDL_Event event;
+	while (SDL_PollEvent(&event))
+	{
+		switch (event.type)
+		{
+		default:
+			break;
+		case SDL_JOYDEVICEADDED:
+		{
+			const int which = event.cdevice.which;
+			for (int joy = 0; joy < 16; joy++)
+			{
+				if (!rdPtr->SDL_Data[joy].connected || rdPtr->SDL_Data[joy].joy_id == which)
+				{
+					//MessageBoxA(nullptr, "Device added.", "SDL message", MB_OK | MB_ICONINFORMATION);
+					OpenJoystick(rdPtr, joy);
+					break;
+				}
+			}
+			break;
+		}
+		case SDL_JOYDEVICEREMOVED:
+		{
+			const int which = event.cdevice.which;
+			for (int joy = 0; joy < 16; joy++)
+			{
+				if (SDL_JoystickInstanceID(rdPtr->SDL_Data[joy].joystick) == which)
+				{
+					//MessageBoxA(nullptr, "Device removed.",	"SDL message", MB_OK | MB_ICONINFORMATION);
+					CloseJoystick(rdPtr, joy);
+					break;
+				}
+			}
+			break;
+		}
+		}
+	}
+	//Update input
+	for (int i = 0; i < 16; i++)
+	{
+		if (rdPtr->SDL_Data[i].connected)
+		{
+			//Axes
+			for (int a = 0; a < rdPtr->SDL_Data[i].num_axes; a++)
+			{
+				rdPtr->SDL_Data[i].axis[a] = SDL_JoystickGetAxis(rdPtr->SDL_Data[i].joystick, a);
+			}
+			//Buttons
+			for (int b = 0; b < rdPtr->SDL_Data[i].num_buttons; b++)
+			{
+				rdPtr->SDL_Data[i].held_buttons_last[b] = rdPtr->SDL_Data[i].held_buttons[b];
+				rdPtr->SDL_Data[i].held_buttons[b] = SDL_JoystickGetButton(rdPtr->SDL_Data[i].joystick, b);
+			}
+			//Hats
+			for (int h = 0; h < rdPtr->SDL_Data[i].num_hats; h++)
+			{
+				rdPtr->SDL_Data[i].hat[h] = SDL_JoystickGetHat(rdPtr->SDL_Data[i].joystick, h);
+			}
+			//Balls
+			for (int l = 0; l < rdPtr->SDL_Data[i].num_balls; l++)
+			{
+				SDL_JoystickGetBall(rdPtr->SDL_Data[i].joystick, l, &rdPtr->SDL_Data[i].ball_x[l], &rdPtr->SDL_Data[i].ball_y[l]);
+			}
+		}
+	}
+	return REFLAG_DISPLAY;
+}
+
+// ----------------
+// DisplayRunObject
+// ----------------
+// Draw the object in the application screen.
+// 
+short WINAPI DLLExport DisplayRunObject(LPRDATA rdPtr)
+{
+	// Ok
+	return 0;
+}
+
+// ----------------
+// PauseRunObject
+// ----------------
+// Enters the pause mode
+// 
+short WINAPI DLLExport PauseRunObject(LPRDATA rdPtr)
+{
+	// Ok
+	return 0;
+}
+
+
+// -----------------
+// ContinueRunObject
+// -----------------
+// Quits the pause mode
+// 
+short WINAPI DLLExport ContinueRunObject(LPRDATA rdPtr)
+{
+	// Ok
+	return 0;
+}
+
+// ============================================================================
+//
+// WINDOWPROC (interception of messages sent to hMainWin and hEditWin)
+//
+// Do not forget to enable the WindowProc function in the .def file if you implement it
+// 
+// ============================================================================
+
+// Get the pointer to the object's data from its window handle
+// Note: the object's window must have been subclassed with a
+// callRunTimeFunction(rdPtr, RFUNCTION_SUBCLASSWINDOW, 0, 0);
+// See the documentation and the Simple Control example for more info.
+/*
+LPRDATA GetRdPtr(HWND hwnd, LPRH rhPtr)
+{
+	return (LPRDATA)GetProp(hwnd, (LPCSTR)rhPtr->rh4.rh4AtomRd);
+}
+
+// Called from the window proc of hMainWin and hEditWin.
+// You can intercept the messages and/or tell the main proc to ignore them.
+//
+/LRESULT CALLBACK DLLExport WindowProc(LPRH rhPtr, HWND hWnd, UINT nMsg, WPARAM wParam, LPARAM lParam)
+{
+	return 0;
+}*/
